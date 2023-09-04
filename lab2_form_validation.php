785fd052304f66ece8cc7c71dc56fa9a3b559a99
suraj <karkisuraj665@gmail.com>
2023-09-04 07:25:47 +0545

    ss

--- lab2_form_validation.php
+++ lab2_form_validation.php
@@ -6,7 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="" method="get" onsubmit="return validateForm()">
+    <?php
+    if(isset($_GET['submit'])){
+        $email2=$_GET['email2'];
+        $emailRegex = "/[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z]{2,3}/";
+        if(preg_match($emailRegex, $email2)){
+            echo "Valid;Signup completed; Insert to db";
+        }else{
+            echo "Invalid Email2";
+        }
+    }
+    ?>
+    <form action="" method="get" >
     <div>SignUp</div>
     <div>Username:<input type="text" name="un" id="un" required minLength="4"></div>
     <div>Password:<input type="password" name="pw" id="pw" required></div>
@@ -15,7 +26,7 @@
     <div id="error"></div>
     <div><input type="submit" value="Signup" name="submit"></div>
 </form>
-<script>
+<!-- <script>
     function validateForm(){
         var emailRegex = /[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z]{2,3}/;
         var email2=document.getElementById("email2").value;
@@ -27,7 +38,7 @@
             return false;
         }
     }
-</script>
+</script> -->
 </body>
 
 </html>
